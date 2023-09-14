db38301e33236b9c1997f9b954178ace7e761d8a
angel0w <94199830+angel0w@users.noreply.github.com>
2023-09-14 22:17:11 +0300

    remove item + clear all lists

--- script.js
+++ script.js
@@ -5,24 +5,39 @@ function addItem(ev) {
     let li = document.createElement('li');
     li.textContent = liItem;
 
+    let deleteElement = document.createElement('a');
+    deleteElement.href = '#';
+    deleteElement.textContent = 'X';
+
     if (liItem !== '') {
+        li.appendChild(deleteElement);
         ulItems.append(li);
         document.getElementById('inputValue').value = '';
     }
+
+    deleteElement.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+    });
+
+    let clearBtn = document.getElementById('clearButton');
+    clearBtn.addEventListener('click', (e) => {
+        let lists = document.querySelectorAll('li');
+        lists.forEach((el) => {
+            el.remove()
+        })
+    })
+
 }
 
 let liElement = Array.from(document.querySelectorAll('#ulList'));
-
-
-liElement.forEach(el => {
+liElement.forEach((el) => {
     el.addEventListener('click', (el) => {
         if (el.target.style.textDecoration == 'line-through') {
-            el.target.style.textDecoration = 'none'
+            el.target.style.textDecoration = 'none';
         } else {
             el.target.style.textDecoration = 'line-through';
         }
-
-    })
-})
+    });
+});
 
 
