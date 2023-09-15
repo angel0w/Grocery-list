f3f4c5e1da8514254f9c170ac4cbd0d1d3930e18
angel0w <94199830+angel0w@users.noreply.github.com>
2023-09-15 23:49:51 +0300

    try to solve -line-through - problem

--- script.js
+++ script.js
@@ -3,41 +3,44 @@ function addItem(ev) {
     let liItem = document.getElementById('inputValue').value;
 
     let li = document.createElement('li');
-    li.textContent = liItem;
+    let p = document.createElement('p');
+    let span = document.createElement('span');
 
-    let deleteElement = document.createElement('a');
-    deleteElement.href = '#';
-    deleteElement.textContent = 'X';
+    p.textContent = liItem;
+    span.textContent = 'X';
 
     if (liItem !== '') {
-        li.appendChild(deleteElement);
+        li.appendChild(p);
+        li.appendChild(span);
         ulItems.append(li);
         document.getElementById('inputValue').value = '';
     }
 
-    deleteElement.addEventListener('click', (e) => {
+    span.addEventListener('click', (e) => {
         e.target.parentElement.remove();
     });
-
-    let clearBtn = document.getElementById('clearButton');
-    clearBtn.addEventListener('click', (e) => {
-        let lists = document.querySelectorAll('li');
-        lists.forEach((el) => {
-            el.remove()
-        })
-    })
-
 }
 
-let liElement = Array.from(document.querySelectorAll('#ulList'));
-liElement.forEach((el) => {
-    el.addEventListener('click', (el) => {
-        if (el.target.style.textDecoration == 'line-through') {
-            el.target.style.textDecoration = 'none';
-        } else {
-            el.target.style.textDecoration = 'line-through';
-        }
+let clearBtn = document.getElementById('clearButton');
+clearBtn.addEventListener('click', (e) => {
+    let lists = document.querySelectorAll('li');
+    lists.forEach((el) => {
+        el.remove();
     });
 });
 
 
+
+
+let pElement = Array.from(document.querySelectorAll('#ulList'));
+pElement.forEach((el) => {
+    el.addEventListener('click', (e) => {
+
+
+        if (e.target.children[0].style.textDecoration == 'line-through') {
+            e.target.children[0].style.textDecoration = 'none';
+        } else {
+            e.target.children[0].style.textDecoration = 'line-through';
+        }
+    });
+});
